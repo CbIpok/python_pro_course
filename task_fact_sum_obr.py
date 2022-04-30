611b3fbbcba664077942e0c50789053e5c09891f
FallenMind <snapgod12@gmail.com>
2022-04-30 20:04:41 +1000

    Signed-off-by: FallenMind <snapgod12@gmail.com>

---
+++ task_fact_sum_obr.py
@@ -0,0 +1,7 @@
+# По данному натуральном n вычислите сумму 1!+2!+3!+...+n!.
+# Ответ подаётся в int, писать в функции run. Ответ подаётся с помощью return x, где x - ваш ответ.
+class TaskFactorialSum:
+    def __init__(self):
+        self.name = "fact_sum"
+    def run(self, n):
+        return None
---
+++ task_fibb_obr.py
@@ -0,0 +1,6 @@
+# Необходимо получить n-ое число Фиббоначи. Ответ подаётся в int, писать в функции run. Ответ подаётся с помощью return x, где x - ваш ответ.
+class TaskFibbonachi:
+    def __init__(self):
+        self.name = "fibb"
+    def run(self, n):
+        return None
